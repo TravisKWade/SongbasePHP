d40d4074a0e4d2b9628977f49cbacb8f0c64b6af
Travis Wade <travis@entrision.com>
2017-03-16 07:28:56 -0700

    song details style

--- songDetails.php
+++ songDetails.php
@@ -49,59 +49,79 @@
 <head>
 	<title> Songbase </title>
 	<link rel="stylesheet" type="text/css" href="styles/songbase.css" />
+	<link rel="stylesheet" type="text/css" href="styles/header.css" />
+	<link rel="stylesheet" type="text/css" href="styles/menu.css" />
+	<link rel="stylesheet" type="text/css" href="styles/songDetails.css" />
+	<link rel="shortcut icon" href="images/favicon.ico">
 	<script src="scripts/jquery.js"></script>
 </head>
 <body>
-	<div>
-		Songbase - SONG DETAILS
+	<div class="header">
+		<div class="title">Songbase - Song Details</div>
+		<div class="logout">
+			<form action="logout.php">
+				<? echo $_SESSION['user']; ?>&nbsp;&nbsp;&nbsp;
+				<input type="submit" value="Log Out" class="logoutButton"/>
+			</form>
+		</div>
+	</div>
+	<div class="menu">
+		<ul>
+			<li><a href="songs.php">Songs</a></li>
+			<li><a href="artists.php">Artists</a></li>
+			<li><a href="albums.php">Albums</a></li>
+			<li><a href="composers.php">Composers</a></li>
+		</ul>
 	</div>
 
-	User: <? echo $_SESSION['user']; ?>
-	<form action="logout.php">
-		<input type="submit" value="Logout" />
-	</form>
-	<ul>
-		<li><a href="songs.php">Songs</a></li>
-		<li><a href="artists.php">Artists</a></li>
-		<li><a href="albums.php">Albums</a></li>
-		<li><a href="composers.php">Composers</a></li>
-	</ul>
-
-	<? echo $song->getName() ?>
-	<br /><br />
-	Song Details
-	<table>
-		<tr>
-			<td>Composer</td>
-			<td><? echo $composer->getName() ?></td>
-		</tr>
-		<tr>
-			<td>Date Written</td>
-			<td><? echo $song->getDateWritten() ?></td>
-		</tr>
-	</table>
-
-	<br /><br />
+	<div class="songTitle">
+		<? echo $song->getName() ?>
+	</div>
+	<div class="songDetails">
+		<h2>Song Details</h2>
+		<table>
+			<tr>
+				<td class="tableHeader">Composer:</td>
+				<td class="tableDetails"><? echo $composer->getName() ?></td>
+			</tr>
+			<tr>
+				<td class="tableHeader">Date Written:</td>
+				<td class="tableDetails"><? echo $song->getDateWritten() ?></td>
+			</tr>
+		</table>
+	</div>
+	<div class="options">
+		<div class="editSong">
+			<form action="editSong.php">
+				<input type="hidden" name="song" value="<? echo $song->getSongID() ?>" />
+		    	<input type="submit" value="Edit Song" class="songbaseButton" />
+			</form>
+		</div>
+		<div class="songLyrics">
+			<?
+			if($lyrics != null) {
+				echo "<form action='lyrics.php'><input type='hidden' name='song' value='{$song->getSongID()}' /><input type='submit' value='Song Lyrics' class='songbaseButton' /></form>";
+			} else {
+				echo "<form action='newLyrics.php'><input type='hidden' name='song' value='{$song->getSongID()}' /><input type='submit' value='Add Song Lyrics' class='songbaseButton' /></form>";
+			}
+			?>
+		</div>
+	</div>
 	<?
-		if($lyrics != null) {
-			echo "<a href='lyrics.php?song={$song->getSongID()}'> Song Lyrics</a>";
-		} else {
-			echo "<a href='newLyrics.php?song={$song->getSongID()}'> Add Song Lyrics</a>";
-		}
 
 		if(count($recordingArray) > 0) {
 	?>
-	<br /><br />
-	Recordings <br />
+	<div class="recordingDetails">
+		<h2>Recordings </h2>
 
-	<table>
-		<tr>
-			<td></td>
-			<td>Artist</td>
-			<td>Album</td>
-			<td>Date</td>
-			<td></td>
-		</tr>
+		<table>
+			<tr>
+				<td></td>
+				<td class="tableHeader">Artist</td>
+				<td class="tableHeader">Album</td>
+				<td class="tableHeader">Date</td>
+				<td></td>
+			</tr>
 	<?
 
 	
@@ -125,7 +145,11 @@
 				}
 
 				echo "<tr>";
-				echo "<td><a href='editRecording.php?rec={$recording->getRecordingID()}'> EDIT </a></td>";
+				echo "<td>";
+				echo "<form action='editRecording.php'>";
+				echo "<input type='hidden' name='rec' value='{$song->getSongID()}' />";
+				echo "<input type='submit' value='' class='editButton' /></form>";
+				echo "</td>";
 				echo "<td><a href='artistDetails.php?art={$artist->getArtistID()}'> {$artist->getName()} </a></td>";
 				echo "<td><a href='albumDetails.php?al={$album->getAlbumID()}'> {$album->getName()} </a></td>";
 				echo "<td>{$recording->getDateRecorded()}</td>";
@@ -134,12 +158,19 @@
 			} 
 	
 	?>
-	</table>
+		</table>
 
 	<?
 		}
 	?>
-	<br /><br />
-	<a href="newRecording.php?song=<? echo $_GET['song'] ?>"> Add Recording </a><br /><br />
+	</div>
+	
+	<div class="newRecording">
+		<form action="newRecording.php">
+			<input type="hidden" name="song" value="<? echo $song->getSongID() ?>" />
+	    	<input type="submit" value="Add New Recording" class="songbaseButton" />
+		</form>
+	</div>
+
 </body>
 </html>
--- songs.php
+++ songs.php
@@ -16,41 +16,97 @@
 <head>
 	<title> Songbase </title>
 	<link rel="stylesheet" type="text/css" href="styles/songbase.css" />
+	<link rel="stylesheet" type="text/css" href="styles/header.css" />
+	<link rel="stylesheet" type="text/css" href="styles/menu.css" />
+	<link rel="stylesheet" type="text/css" href="styles/songs.css" />
+	<link rel="shortcut icon" href="images/favicon.ico">
 	<script src="scripts/jquery.js"></script>
 </head>
 <body>
-	<div>
-		Songbase - SONG LIST
+	<div class="header">
+		<div class="title">Songbase - Song List</div>
+		<div class="logout">
+			<form action="logout.php">
+				<? echo $_SESSION['user']; ?>&nbsp;&nbsp;&nbsp;
+				<input type="submit" value="Log Out" class="logoutButton"/>
+			</form>
+		</div>
 	</div>
+	<div class="menu">
+		<ul>
+			<li><a href="songs.php">Songs</a></li>
+			<li><a href="artists.php">Artists</a></li>
+			<li><a href="albums.php">Albums</a></li>
+			<li><a href="composers.php">Composers</a></li>
+		</ul>
+	</div>
+	<div class="newSong">
+		<form action="newSong.php">
+	    	<input type="submit" value="New Song" class="songbaseButton" />
+		</form>
+	</div>
+	<div class="shortcut">
+		<a href="#num">#</a>
+		<a href="#A">A</a>
+		<a href="#B">B</a>
+		<a href="#C">C</a>
+		<a href="#D">D</a>
+		<a href="#E">E</a>
+		<a href="#F">F</a>
+		<a href="#G">G</a>
+		<a href="#H">H</a>
+		<a href="#I">I</a>
+		<a href="#J">J</a>
+		<a href="#K">K</a>
+		<a href="#L">L</a>
+		<a href="#M">M</a>
+		<a href="#N">N</a>
+		<a href="#O">O</a>
+		<a href="#P">P</a>
+		<a href="#Q">Q</a>
+		<a href="#R">R</a>
+		<a href="#S">S</a>
+		<a href="#T">T</a>
+		<a href="#U">U</a>
+		<a href="#V">V</a>
+		<a href="#W">W</a>
+		<a href="#X">X</a>
+		<a href="#Y">Y</a>
+		<a href="#Z">Z</a>
+	</div>
+	<div class="songList">
+		<h1>Song List</h1>
 
-	User: <? echo $_SESSION['user']; ?>
-	<form action="logout.php">
-		<input type="submit" value="Logout" />
-	</form>
-	<ul>
-		<li><a href="songs.php">Songs</a></li>
-		<li><a href="artists.php">Artists</a></li>
-		<li><a href="albums.php">Albums</a></li>
-		<li><a href="composers.php">Composers</a></li>
-	</ul>
-	<br /><br />
-	<a href="newSong.php">New Song</a> <br /><br />
-	Song List <br />
+		<? 
+			if ($rs != null) {
+				$rows = $rs->num_rows;
+				if ($rows > 0) {
+					$currentLetter = "";
+					while($row = $rs->fetch_assoc()) {
+						$song = new Song($row);
+						$firstLetter = substr($song->getName(), 0, 1);
 
-	<? 
-		if ($rs != null) {
-			$rows = $rs->num_rows;
-			if ($rows > 0) {
-				while($row = $rs->fetch_assoc()) {
-					$song = new Song($row);
-					echo "<a href='editSong.php?song={$song->getSongID()}'> EDIT </a> -- <a href='songDetails.php?song={$song->getSongID()}'> {$song->getName()} </a><br />";
-				}
+						if($firstLetter != $currentLetter) {
+							if (is_numeric($firstLetter) && is_numeric($currentLetter)) {
+								// we do nothing here, we're on the number songs
+							} else if (is_numeric($firstLetter) && !is_numeric($currentLetter)) {
+								echo "<div class='letterBreak' id='num'><h1>#</h1></div>";
+								$currentLetter = $firstLetter;
+							} else {
+								echo "<div class='letterBreak' id='{$firstLetter}'><h1>{$firstLetter}</h1></div>";
+								$currentLetter = $firstLetter;
+							}
+						}
+
+						echo "<a href='songDetails.php?song={$song->getSongID()}'> {$song->getName()} </a><br />";
+					}
+				} else {
+					echo "There are no songs yet";
+				} 
 			} else {
 				echo "There are no songs yet";
-			} 
-		} else {
-			echo "There are no songs yet";
-		}
-	?>
+			}
+		?>
+	</div>
 </body>
 </html>
